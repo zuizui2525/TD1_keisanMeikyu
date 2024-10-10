1fca9370947dc6a26b24dc697bcd9b40c42b5c1f
k024g0006@g.neec.ac.jp <k024g0006@g.neec.ac.jp>
2024-10-10 12:48:38 +0900

    enum.hの追加 変数sceneの追加

--- main.cpp
+++ main.cpp
@@ -9,6 +9,13 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
+	///// 画像の読み取り /////
+
+
+	///// 初期化 /////
+	//画面切り替え
+	int scene = 0;
+
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
 	char preKeys[256] = {0};
@@ -22,21 +29,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		memcpy(preKeys, keys, 256);
 		Novice::GetHitKeyStateAll(keys);
 
-		///
-		/// ↓更新処理ここから
-		///
-
-		///
-		/// ↑更新処理ここまで
-		///
-
-		///
-		/// ↓描画処理ここから
-		///
-
-		///
-		/// ↑描画処理ここまで
-		///
+		///// ↓↓処理↓↓ /////
+		
+		///// ↑↑処理↑↑ /////
 
 		// フレームの終了
 		Novice::EndFrame();
--- structure.h
+++ structure.h
@@ -5,3 +5,33 @@ struct Vector2 {
 	float y;
 };
 
+struct Grid {
+	int beside;
+	int vertical;
+};
+
+struct Line {
+	Vector2 startPos;
+	Vector2 endPos;
+	int color;
+};
+
+struct Box {
+	Vector2 pos;
+	float width;
+	float height;
+	float angle;
+	int color;
+	FillMode shape;
+};
+
+struct Circle {
+	Vector2 pos;
+	Vector2 radius;
+	float angle;
+	int color;
+	FillMode shape;
+};
+
+
+
